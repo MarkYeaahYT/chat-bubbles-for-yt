eeb66f24769483c83fae6335f908f775467fc849
Arif Dian Suranta <arifdian000@gmail.com>
2022-05-23 10:20:33 +0700

    add sound effect

--- src/bubble-input.jsx
+++ src/bubble-input.jsx
@@ -7,6 +7,8 @@ const BubbleInput = ({ onChange, onSubmit, value }) => {
 
   const handleKeyDown = e => {
     if (e.key === 'Enter') {
+      const audio = new Audio("/assets/audio/turquoise/press/ENTER.mp3");
+      audio.play()
       onSubmit && onSubmit()
       e.preventDefault()
       setSubmitted(true)
@@ -15,7 +17,17 @@ const BubbleInput = ({ onChange, onSubmit, value }) => {
         refEditable.current.innerText = ''
         setSubmitted(false)
       }, 10)
+    }else if(e.key === 'Backspace'){
+      const audio = new Audio("/assets/audio/turquoise/press/BACKSPACE.mp3");
+      audio.play()
+    }else if(e.key === ' '){
+      const audio = new Audio("/assets/audio/turquoise/press/SPACE.mp3");
+      audio.play()
+    }else{
+      const audio = new Audio("/assets/audio/turquoise/press/GENERIC_R0.mp3");
+      audio.play()
     }
+    
   }
   const handleBlur = useCallback(() => {
     const { current: elDiv } = refEditable
